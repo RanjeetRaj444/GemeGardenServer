37b8eb558ac4ab3feb58e6d9709d2f188b482a7f
sharvarihupare-369 <sharvarihupare@gmail.com>
2023-06-18 14:08:23 +0530

    add

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ server.use(cors({
 }));
 server.use(middlewares)
 server.use(router)
-app.use((req, res, next) => {
+server.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
 server.get('/', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
     res.send("Server is working")
